b378f6013273ffcf8abe6956f4c162b76a3ac786
liuhanyu <1218907511@qq.com>
2021-06-19 20:13:26 +0800

    1

--- 10/division.py
+++ 10/division.py
@@ -4,3 +4,8 @@ try:
     print(5/0)
 except ZeroDivisionError:
     print("You can't divide by zero!")
+
+print("Give me two numbers, I'll divide them.")
+print("Print q to exit.")
+number1 = input("First number: ")
+number2 = input("Second number: ")
