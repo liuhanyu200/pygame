7bd82d6eb90cc76c4ed9a7023da54158c6414344
liuhanyu <1218907511@qq.com>
2021-06-30 15:04:02 +0800

    自定义移速

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -3,3 +3,4 @@ class Settings():
         self.screen_width = 1000
         self.screen_height = 650
         self.bg_color = (230, 230, 230)
+        self.ship_speed_factor = 0.2
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -1,22 +1,24 @@
 import pygame
 
-
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         self.screen = screen
         self.image = pygame.image.load('./images/ship.png')
         self.rect = self.image.get_rect()
+        self.ai_settings = ai_settings
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
         self.moving_right = False
         self.moving_left = False
+        self.center = float(self.rect.centerx)
 
     def update(self):
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center
 
     def bliteme(self):
         self.screen.blit(self.image, self.rect)
